114b35773ebfda852c81c02824ed828ff5e983e2
Anthony Howl <anthonyhowl@protonmail.ch>
2018-09-13 16:40:17 -0700

    Logic for rental pre-SpartanBot

--- cli.js
+++ cli.js
@@ -5,15 +5,57 @@ vorpal
   .action(function(args, callback) {
     this.log('bars');
     callback();
+    hr.ask(function(answer){
+      console.log(answer)
+    })
   });
  
+  vorpal.command('rent')
+  .action(function(args, cb){
+    const self = this;
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'hashrate',
+        default: '5G',
+        message: 'How much Hashrate would you like to rent?'
+      },
+      {
+        type: 'input',
+        name: 'duration',
+        default: '1000',
+        message: 'How long would you like SpartanBot to run?'
+      },
+    ], 
+    function(result){
+      return self.prompt({
+          type: 'confirm',
+          name: 'confirm',
+          message: 'Would you like to rent 2GH for $25.31 (25 Miners)?'
+        })
+      if (!result.continue) {
+        self.log('Good move.');
+        cb();
+      } else {
+        self.log('Time to dust off that resume.');
+      }
+    });
+  });
+
   vorpal
-  .command('rent 5G for 24HR', '5Gs will be rented for 24HR.')
+  .command('foo <requiredArg> [optionalArg]')
+  .option('-v, --verbose', 'Print foobar instead.')
+  .description('Outputs "bar".')
+  .alias('foosball')
   .action(function(args, callback) {
-    this.log('bars');
+    if (args.options.verbose) {
+      this.log('foobar');
+    } else {
+      this.log('bar');
+    }
     callback();
   });
 
 vorpal
-  .delimiter('Spartan-Cli$')
+  .delimiter('spartan-cli$')
   .show();
