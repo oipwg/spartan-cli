b464430d78a62efebe3a5296d4cb3ef688e6a20d
Anthony Howl <anthonyhowl@protonmail.ch>
2018-09-14 13:34:07 -0700

    SpartanBot hooked / Pre- Vorpal link and API spawn

--- cli.js
+++ cli.js
@@ -1,5 +1,39 @@
 const vorpal = require('vorpal')();
- 
+const { SpartanBot } = require('spartanbot')
+
+let spartan = new SpartanBot();
+
+let setup_provider, rent_manual;
+
+async function SetupFunction() {
+  setup_provider = await spartan.setupRentalProvider({ 
+    type: "MiningRigRentals",
+    api_key: "my-api-key",
+    api_secret: "my-api-secret"
+  });
+  // 1 = 1MH
+  // 1000 = 1GH
+  let hashrate = 1000
+  // 60 seconds * 60 minutes * 24 hours = one day
+  let duration_seconds = 60 * 60 * 24 
+  rent_manual = await spartan.manualRental(hashrate, duration_seconds, async (prepurchase_info) => {
+    console.log(prepurchase_info)
+    // Prompt the user to figure out if they want to confirm the purchase
+  
+  
+    // return `true` if the user is ok buying, and `false` if the user is not okay purchasing
+    return true
+  })
+}
+SetupFunction()
+  .then( () => {
+    console.log(`SP: ${JSON.stringify(setup_provider, null, 4)} and RM: ${JSON.stringify(rent_manual, null, 4)}`)
+  })
+  .catch( err => {
+    console.log('err: ', err)
+  })
+
+
 vorpal
   .command('setup', 'Users API key will be set.')
   .action(function(args, callback) {
@@ -11,7 +45,7 @@ vorpal
   });
  
   vorpal.command('rent')
-  .action(function(args, cb){
+  .action(function(args, callback){
     const self = this;
     return this.prompt([
       {
@@ -31,9 +65,9 @@ vorpal
       return self.prompt({
           type: 'confirm',
           name: 'confirm',
-          message: 'Would you like to rent 2GH for $25.31 (25 Miners)?'
+          message: `Would you like to rent ${"2GH"} for ${"$25.31"} (${25} Miners)?`
         })
-      if (!result.continue) {
+      if (!result.confirm) {
         self.log('Good move.');
         cb();
       } else {
