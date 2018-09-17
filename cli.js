06a1b66f08c39edf3314d3cb8d8b4be11abe163c
Anthony Howl <anthonyhowl@protonmail.ch>
2018-09-17 12:57:44 -0700

    Pre-Test & Pre-Config and manual Rent update

--- cli.js
+++ cli.js
@@ -1,95 +1,47 @@
 const vorpal = require('vorpal')();
 const { SpartanBot } = require('spartanbot')
+var bytes = require('bytes');
 
 let spartan = new SpartanBot();
 
-let setup_provider, rent_manual;
-
-async function SetupFunction() {
-  setup_provider = await spartan.setupRentalProvider({ 
-    type: "MiningRigRentals",
-    api_key: "my-api-key",
-    api_secret: "my-api-secret"
-  });
-  // 1 = 1MH
-  // 1000 = 1GH
-  let hashrate = 1000
-  // 60 seconds * 60 minutes * 24 hours = one day
-  let duration_seconds = 60 * 60 * 24 
-  rent_manual = await spartan.manualRental(hashrate, duration_seconds, async (prepurchase_info) => {
-    console.log(prepurchase_info)
-    // Prompt the user to figure out if they want to confirm the purchase
-  
-  
-    // return `true` if the user is ok buying, and `false` if the user is not okay purchasing
-    return true
-  })
-}
-SetupFunction()
-  .then( () => {
-    console.log(`SP: ${JSON.stringify(setup_provider, null, 4)} and RM: ${JSON.stringify(rent_manual, null, 4)}`)
-  })
-  .catch( err => {
-    console.log('err: ', err)
-  })
-
-
 vorpal
-  .command('setup', 'Users API key will be set.')
-  .action(function(args, callback) {
-    this.log('bars');
-    callback();
-    hr.ask(function(answer){
-      console.log(answer)
-    })
-  });
- 
-  vorpal.command('rent')
-  .action(function(args, callback){
-    const self = this;
-    return this.prompt([
-      {
-        type: 'input',
-        name: 'hashrate',
-        default: '5G',
-        message: 'How much Hashrate would you like to rent?'
-      },
-      {
-        type: 'input',
-        name: 'duration',
-        default: '1000',
-        message: 'How long would you like SpartanBot to run?'
-      },
-    ], 
-    function(result){
-      return self.prompt({
-          type: 'confirm',
-          name: 'confirm',
-          message: `Would you like to rent ${"2GH"} for ${"$25.31"} (${25} Miners)?`
-        })
-      if (!result.confirm) {
-        self.log('Good move.');
-        cb();
-      } else {
-        self.log('Time to dust off that resume.');
-      }
-    });
-  });
+	.delimiter('spartan-cli$')
+	.command('rent')
+	.action(async function (args, cb) {
+		const self = this;
+		let questions = [
+			{
+				type: 'input',
+				name: 'hashrate',
+				default: '5G',
+				message: 'How much Hashrate would you like to rent? '
+			},
+			{
+				type: 'input',
+				name: 'duration',
+				default: '1d, 1hr',
+				message: 'How long would you like SpartanBot to run? '
+			}
+		];
 
-  vorpal
-  .command('foo <requiredArg> [optionalArg]')
-  .option('-v, --verbose', 'Print foobar instead.')
-  .description('Outputs "bar".')
-  .alias('foosball')
-  .action(function(args, callback) {
-    if (args.options.verbose) {
-      this.log('foobar');
-    } else {
-      this.log('bar');
-    }
-    callback();
-  });
+		var answers = await this.prompt(questions);
 
-vorpal
-  .delimiter('spartan-cli$')
-  .show();
+		hashrate = bytes(answers.hashrate);
+		duration = answers.duration;
+
+		var rent_manual = await spartan.manualRental(hashrate, duration, async (prepurchase_info) => {
+			//self.log("prepurchase", prepurchase_info)
+			var confirm_purchase = await self.prompt({
+				type: 'confirm',
+				name: 'confirm',
+				message: 'Do you want to rent 13 miners for $' + prepurchase_info.total_cost.toFixed(2) + '?'
+			})
+
+			return confirm_purchase
+		})
+
+		self.log("rental status: ", rent_manual)
+	});
+	 
+vorpal.show()
+	
