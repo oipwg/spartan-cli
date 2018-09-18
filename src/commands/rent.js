14b331b875e5b44d4cb4c7e9ab9ad62d86e9c489
Sky Young <sky@alexandria.io>
2018-09-17 18:29:24 -0700

    Display rental info on rental confirm/success

--- src/commands/rent.js
+++ src/commands/rent.js
@@ -14,37 +14,44 @@ export default function(vorpal, options){
 			{
 				type: 'input',
 				name: 'hashrate',
-				default: '5G',
-				message: vorpal.chalk.yellow('How much Hashrate would you like? ')
+				default: '1GH',
+				message: vorpal.chalk.yellow('How much Hashrate would you like to rent (i.e. 250MH, 1GH)? ')
 			},
 			{
 				type: 'input',
 				name: 'duration',
-				default: '1h',
+				default: '6h',
 				message: vorpal.chalk.yellow('How long would you like to rent your miner? ')
 			}
 		];
 
 		var answers = await this.prompt(questions);
 
-		console.log(answers)
-
 		let converted_hashrate = convertHumanHashrateToMH(answers.hashrate);
 		let converted_duration = convertHumanTimeToSeconds(answers.duration);
+
+		self.log(vorpal.chalk.cyan("Searching for miners..."))
+
 		var rent_manual = await spartan.manualRental(converted_hashrate, converted_duration, async (prepurchase_info) => {
-			self.log("prepurchase", prepurchase_info)
 			var confirm_purchase = await self.prompt({
 				type: 'confirm',
 				name: 'confirm',
-				message: vorpal.chalk.yellow('Do you want to rent ' + prepurchase_info.total_rigs + ' miners for $' + prepurchase_info.total_cost.toFixed(2) + '?')
+				default: false,
+				message: vorpal.chalk.yellow('Do you want to rent ' + prepurchase_info.total_rigs + ' miner(s) (' + (prepurchase_info.total_hashrate/1000).toFixed(2) + ' GH) for $' + prepurchase_info.total_cost + '?')
 			})
 			if (confirm_purchase.confirm){
-				return self.log(vorpal.chalk.green("rental status: ", rent_manual.info + " !"))
-				} else {
-					this.log(vorpal.chalk.red(" Rental was aborted"))
-				}
-		
-		
-			})
+				self.log(vorpal.chalk.cyan("Renting miners..."))
+			} else {
+				this.log(vorpal.chalk.red("Rental was aborted"))
+			}
+
+			return confirm_purchase.confirm
+		})
+
+		if (rent_manual.success){
+			self.log(vorpal.chalk.green(`Successfully rented ${rent_manual.total_rigs_rented} miner(s) (${(rent_manual.total_hashrate/1000).toFixed(2)} GH) for $${rent_manual.total_cost}!`))
+		} else {
+			return this.log(vorpal.chalk.red("Unable to rent Miners! " + rent_manual))
+		}
 	});
 }
