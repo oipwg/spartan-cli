56d9b8e5c360dad585c2c203460e1495692c5ecd
orpheus <ryanachacon@gmail.com>
2018-09-26 10:28:15 -0700

    added a reInit and main function

--- src/cli.js
+++ src/cli.js
@@ -2,13 +2,21 @@ import Vorpal from 'vorpal'
 import { SpartanBot } from 'spartanbot'
 import commands from './commands'
 
-let spartan_cli = Vorpal()
+let spartan_cli = Vorpal();
 
 let spartan = new SpartanBot();
+const reInitialize = (settings) => {
+	spartan = new SpartanBot(settings);
+	__main__(spartan)
+};
 
-spartan_cli
-	.delimiter(spartan_cli.chalk.magenta('spartan-cli$'))
-	.use(commands, {SpartanBot: spartan})
-	.history('spartancli-history')
-	 
-spartan_cli.show()
+const __main__ = (spartan) => {
+	spartan_cli
+		.delimiter(spartan_cli.chalk.magenta('spartan-cli$'))
+		.use(commands, {SpartanBot: spartan, reInitialize})
+		.history('spartancli-history');
+
+	spartan_cli.show();
+};
+
+__main__();
