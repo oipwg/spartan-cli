cb97a7da2163d8438f6b100d1bd0684ae19d48cf
orpheus <ryanachacon@gmail.com>
2018-10-06 13:36:32 -0700

    refactor/cleanup pool list - call from api

--- src/commands/Pools/list.js
+++ src/commands/Pools/list.js
@@ -4,91 +4,99 @@ export default function(vorpal, options){
 	let spartan = options.SpartanBot;
 
 	vorpal
-		.command('pools')
+		.command('pool list')
 		.description('List of all current pools')
 		.alias('pl')
 		.action(async function(args) {
 			const self = this;
+			const exit = vorpal.chalk.red(`exit`)
+
 			await spartan._deserialize;
 			if (spartan.getRentalProviders().length === 0) {
 				self.log(vorpal.chalk.red(`No rental providers found! Add one by typing: rentalprovider add`));
 				return
 			}
 
-			let pools;
+			let _pools;
 			try {
-				pools = await spartan.returnPools()
+				_pools = await spartan.getPools()
 			} catch (err) {
-				self.log(vorpal.chalk.red("Failed to get pools: ", err))
+				self.log(vorpal.chalk.red("Failed to get pools from MRR API: ", err))
 				return
 			}
 
 			let fmtPoolArray = [];
-			for (let pool of pools) {
+			for (let pool of _pools) {
 				fmtPoolArray.push(fmtPool(serPool(pool), vorpal))
 			}
 
-			self.log(vorpal.chalk.white('Your pools across all market providers: '))
-			let pool = await self.prompt({
+			let promptPools = await self.prompt({
 				type: 'list',
-				message: 'Pick a pool: ',
-				name: 'pool',
-				choices: [...fmtPoolArray, 'exit/return']
+				message: 'Select a pool to manage:',
+				name: 'choice',
+				choices: [...fmtPoolArray, exit]
 			});
 
-			let chosenPool = pool.pool;
+			let chosenPool = promptPools.choice;
 			let poolObj = {};
-			for (let pool of pools) {
+			for (let pool of _pools) {
 				poolObj[fmtPool(serPool(pool), vorpal)] = pool.id
 			}
 			let poolid = poolObj[chosenPool];
-
-			if (chosenPool !== 'exit/return') {
-				let poolOptions = await self.prompt({
-					type: 'list',
-					message: 'Would you like to delete this pool or set it to active?',
-					name: 'option',
-					choices: ['Set to Active', 'Delete', 'exit']
-				})
-				let poolOpt = poolOptions.option;
-				if (poolOpt !== 'exit') {
-					if (poolOpt === 'Set to Active') {
-						//set pool to top priorty under active profile for mrr
-						//set pool to active for nicehash
-						let providers = spartan.getRentalProviders()
-						for (let provider of providers) {
-							if (provider.getInternalType() === "MiningRigRentals") {
-								provider.setActivePool(poolid)
-								let profileID = provider.returnActivePoolProfile()
-								let poolToUse;
-								for (let pool of pools) {
-									if (pool.id === poolid) {
-										poolToUse = {...pool, profileID, priority: 0}
-									}
-								}
-								let res;
-								try {
-									res = await provider.addPoolToProfile(poolToUse)
- 								} catch (err) {
-									throw new Error(`Failed to add pool to main profile: ${err}`)
-								}
-								if (res.success)
-									self.log(vorpal.chalk.yellow('Pool is active'))
-								else
-									self.log(vorpal.chalk.red(JSON.stringify(res, null, 4)))
-							}
-							if (provider.getInternalType() === "NiceHash") {
-								provider.setActivePool(poolid)
-							}
-						}
-					}
-					if (poolOpt === 'Delete') {
-						//remove pool across all providers
-						let success = await spartan.deletePool(poolid)
-						self.log('delete success', success)
-					}
-					spartan.serialize()
+			let _pool;
+			for (let pool of _pools) {
+				if (pool.id === poolid) {
+					_pool = pool
 				}
 			}
+
+			if (chosenPool === exit)
+				return
+
+			let promptPoolCommands = await self.prompt({
+				type: 'list',
+				message: vorpal.chalk.yellow('Choose an option:'),
+				name: 'choice',
+				choices: ['Set to Active', 'Delete', exit]
+			})
+			let chosenCommand = promptPoolCommands.choice;
+
+			if (chosenCommand === exit)
+				return
+
+			if (chosenCommand === 'Set to Active') {
+				//set pool to top priorty under active profile for mrr
+				let providers = spartan.getRentalProviders()
+				for (let provider of providers) {
+					if (provider.getInternalType() === "MiningRigRentals") {
+						provider.setActivePool(poolid)
+						let profileID = provider.returnActivePoolProfile()
+						let extendedPool = {..._pool, profileID, priority: 0}
+						let res;
+						try {
+							res = await provider.addPoolToProfile(extendedPool)
+                        } catch (err) {
+							throw new Error(`Failed to add pool to main profile: ${err}`)
+						}
+
+						if (res.success)
+							self.log(vorpal.chalk.yellow('Pool is active'))
+						else
+							self.log(vorpal.chalk.red(`Failed to add pool to MRR profile: ${JSON.stringify(res, null, 4)}`))
+					}
+					//set pool to active for nicehash
+					if (provider.getInternalType() === "NiceHash") {
+						provider.setActivePool(poolid)
+					}
+				}
+			}
+
+			if (chosenCommand === 'Delete') {
+				//remove pool across all providers
+				let success = await spartan.deletePool(poolid)
+				self.log('delete success', success)
+			}
+
+			spartan.serialize()
 		});
 }
